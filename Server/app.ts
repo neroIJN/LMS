e298bf913545019f2ddcbe37756ce371bdcc4231
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-07-07 15:40:19 +0530

    Implementing ErrorHandler

--- Server/app.ts
+++ Server/app.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import {ErrorMiddleware} from "./middleware/error";
 
 app.use(express.json({limit:"50mb"}));
 
@@ -24,4 +24,6 @@ app.all("*",(req:Request,res:Response,next:NextFunction)=>{
     const err = new Error(`Route ${req.originalUrl} not found`) as any;
     err.statusCode = 404;
     next(err);
-})
+});
+
+app.use(ErrorMiddleware);
