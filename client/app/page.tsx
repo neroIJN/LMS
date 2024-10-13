c24df2cc0d9fb64ab224e0a18a326001a2f3ea1c
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-10-13 14:15:51 +0530

    adding admin

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,25 +4,34 @@ import Heading from "./Utils/Heding";
 import Header from "./components/Header";   
 import Hero from "./components/Route/Hero";
 
-interface Props{};
-const Page:FC<Props> = (props)=>{
+interface Props {}
+
+const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
-  const[activeItem,setActiveItem] = useState(0);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+
   return (
     <div>
-      <Heading 
-      title="EduFlow"
-      description="EEduFlow is a platform offering interactive learning experiences. It enables seamless course management, collaboration, and assessments, making education engaging and effective for both students and teachers."
-      keywords="Programming ,MERN , ML, Redux"
+      <Heading
+        title="ELearning"
+        description="ELearning is a platform for students to learn and get help from teachers"
+        keywords="Prograaming,MERN,Redux,Machine Learning"
       />
       <Header
-      open={open}
-      setOpen={setOpen}
-      activeItem={activeItem}
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
       />
-      <Hero/>
+      <Hero />
+      <Courses />
+      <Reviews />
+      <FAQ />
+      <Footer />
     </div>
-  )
+  );
 };
 
 export default Page;
